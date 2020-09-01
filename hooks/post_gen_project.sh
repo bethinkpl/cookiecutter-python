7e5b9c76311bc48fc31ae57b28debc2e6720c15d
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-01 15:38:01 +0200

    fix? post_gen_project.sh
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,6 +5,7 @@ git add .
 git commit -m "initial commit"
 git branch -M master
 {%- if cookiecutter.create_remote -%}
+echo git@github.com:{{ cookiecutter.github_org_name }}/{{ cookiecutter.project_name }}.git
 git remote add origin git@github.com:{{ cookiecutter.github_org_name }}/{{ cookiecutter.project_name }}.git
 git push -u origin master
 {% endif %}

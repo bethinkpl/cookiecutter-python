8f7a97214dc217d145127efef8f2971e5697d81f
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-01 14:24:45 +0200

    fix post_gen_project.sh
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 poetry update
 git init
-git add
+git add .
 git commit -m "initial commit"
 git remote add origin git@github.com:{{ cookiecutter.github_org_name }}/{{ cookiecutter.project_name }}.git
 git push -u origin master

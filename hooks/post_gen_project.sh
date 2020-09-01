0310dcf76a192251930368d9318653a3875f2fc0
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-01 14:37:39 +0200

    Remote project generation parametrized.
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,5 +3,7 @@ poetry update
 git init
 git add .
 git commit -m "initial commit"
+{%- if cookiecutter.create_remote == true -%}
 git remote add origin git@github.com:{{ cookiecutter.github_org_name }}/{{ cookiecutter.project_name }}.git
 git push -u origin master
+{% endif %}

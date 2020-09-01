1042be7a39dc5163be8f89e8515141897b9e3a43
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-01 15:29:20 +0200

    fix? template
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,7 +3,7 @@ poetry update
 git init
 git add .
 git commit -m "initial commit"
-{%- if cookiecutter.create_remote == true -%}
+{%- if cookiecutter.create_remote -%}
 git remote add origin git@github.com:{{ cookiecutter.github_org_name }}/{{ cookiecutter.project_name }}.git
 git push -u origin master
 {% endif %}

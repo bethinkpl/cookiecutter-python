abde18ddfe31fefb27d57cb82cf7a4d367e6f380
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-03 16:38:59 +0200

    "if" block fixed.
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,7 +6,7 @@ git init
 git add .
 git commit -m "initial commit"
 git branch -M master
-{%- if cookiecutter.create_remote -%}
+{%- if cookiecutter.create_remote == true -%}
 echo "Creating repo in git@github.com:{{ cookiecutter.github_org_name }}/{{ cookiecutter.project_name }}.git"
 git remote add origin git@github.com:{{ cookiecutter.github_org_name }}/{{ cookiecutter.project_name }}.git
 git push -u origin master
